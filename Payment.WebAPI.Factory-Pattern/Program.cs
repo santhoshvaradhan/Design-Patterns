5e9cbeaf962c5b5ec5b31d6c8a279b6b6a894f7d
santhoshvaradhan <santhosh.varadhan02@gmail.com>
2026-03-19 19:40:46 +0530

    Add DebitCardPayment feature

--- Payment.WebAPI.Factory-Pattern/Program.cs
+++ Payment.WebAPI.Factory-Pattern/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<PaymentFactory>();
 builder.Services.AddKeyedScoped<IPayment, UPIPayment>("UPI");
 builder.Services.AddKeyedScoped<IPayment, CreditCardPayment>("CreditCard");
+builder.Services.AddKeyedScoped<IPayment, DebitCardPayment>("DebitCard");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Payment.WebAPI.Factory-Pattern/Services/DebitCardPayment.cs
@@ -0,0 +1,16 @@
+﻿using Payment.WebAPI.Factory_Pattern.Models;
+
+namespace Payment.WebAPI.Factory_Pattern.Services
+{
+    public class DebitCardPayment : IPayment
+    {
+        public async Task<PaymentResponseBuilder> PayAsync(PaymentRequest paymentRequest)
+        {
+            // Simulate some processing time
+            await Task.Delay(1000);
+            // Here you would implement the actual logic to process the debit card payment
+            // For demonstration purposes, we'll just return a successful response
+            return new PaymentResponseBuilder().WithPaymentType(paymentRequest.Type).WithPaymentStatus("Success");
+        }
+    }
+}
